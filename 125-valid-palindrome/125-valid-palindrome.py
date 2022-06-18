1b42f281c666966029766e40d94ddc0680b8c648
Muminur Rahman <r.muminur@gmail.com>
2022-06-18 22:07:00 +0600

    Time: 69 ms (52.47%), Space: 19.2 MB (8.82%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,9 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:
             return False
-        s = s.lower()
-        s = ''.join(ch.lower() for ch in s if ch.isalnum())
-        s = s.replace(" ", "")
+        s = ''.join(ch.lower().replace(" ", "") for ch in s if ch.isalnum())
     
         return s == s[::-1]
             
