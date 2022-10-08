a91250e37d7fbf240b5abff4b31501f32cf845ec
Muminur Rahman <r.muminur@gmail.com>
2022-10-08 22:26:48 +0600

    Time: 175 ms (39.16%), Space: 15.5 MB (9.80%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,18 +1,48 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         """
-        the complexity is O(n)
+        the complexity is O(log n) with binary 
         """
-        result = [-1, -1]
+        first_occurence = self.findBound(nums, target, True)
+        if (first_occurence == -1):
+            return [-1, -1]
         
-        try:
-            result[0] = nums.index(target)
-        except ValueError:
-            return result
+        last_occurence = self.findBound(nums, target, False)
         
-        for i in range(result[0], len(nums)):
-            if nums[i] == target:
-                result[1] = i
+        return [first_occurence, last_occurence]
+    
+    
+    def findBound(self, nums: List[int], target: int, isFirst: bool) -> int:
         
-        return result
+        length_of_nums = len(nums)
+        index_start = 0
+        index_end = length_of_nums - 1
+        
+        while index_start <= index_end:
+            index_mid = (index_start + index_end) // 2   
+            
+            if nums[index_mid] == target:
+                
+                if isFirst:
+                    # This means we found our lower bound.
+                    if index_mid == index_start or nums[index_mid - 1] < target:
+                        return index_mid
+
+                    # Search on the left side for the bound.
+                    index_end = index_mid - 1
+                else:
+                    
+                    # This means we found our upper bound.
+                    if index_mid == index_end or nums[index_mid + 1] > target:
+                        return index_mid
+                    
+                    # Search on the right side for the bound.
+                    index_start = index_mid + 1
+            
+            elif nums[index_mid] > target:
+                index_end = index_mid - 1
+            else:
+                index_start = index_mid + 1
+        
+        return -1 
         
